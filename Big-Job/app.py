9f46f83dcb7de17ee56b28544fb127e0fe23663a
aimen <cherif_aymenn@hotmail.com>
2024-01-07 23:14:13 +0100

    better visualization

--- Big-Job/app.py
+++ Big-Job/app.py
@@ -56,13 +56,13 @@ def emission_contributions():
 def annual_emissions():
     electricity_consumed = (request.form['countryInput'])
     if electricity_consumed == "":
-        return render_template('co2.html', error="Please select a correct consumption value (in kW)")
+        return render_template('co2.html', error="Error in your selection: Please select a correct consumption value (in kW)")
 
     electricity_consumed = float(electricity_consumed)
     selected_country = request.form['countrySelect']
 
     if selected_country == "all":
-        return render_template('co2.html', error= "Please select a single country")
+        return render_template('co2.html', error= "Error in your selection: Please select a single country")
 
     df = emission_contributions()
     country_df = df[df["country"] == selected_country]
@@ -73,8 +73,8 @@ def annual_emissions():
     tree_abs = 25
     number_of_trees = annual_emissions / tree_abs
 
-    return render_template('co2.html', result=str(annual_emissions) , selected_country=selected_country,
-                           trees = number_of_trees)
+    return render_template('co2.html', result=str(annual_emissions) + str("kg of CO2") , selected_country=selected_country,
+                           trees = number_of_trees, error= "Task Completed. You can try another country by going back to emissions.")
 
 
 
